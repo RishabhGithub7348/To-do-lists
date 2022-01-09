ff145063b8f390329e0001105bbb0362b823c65d
RishabhGithub7348 <rishabhmaurya7654@gmail.com>
2022-01-09 22:19:18 +0530

    To_do_list

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Todolist  from "./Todolist";
+
+const App = () => {
+    const [inputList, setInputList] =  useState("");
+    const [items, setItems] = useState([]);
+    const itemEvent = (event) => {
+       setInputList(event.target.value);
+    };
+    const listOfItems = () => {
+        setItems( (oldItems) => {
+            return [...oldItems, inputList];
+        })
+        setInputList("");
+    };
+    const deleteItem = (id) => {
+        setItems((oldItems) => {
+            return oldItems.filter((arrElem, index) => {
+                return index !== id;
+            });
+
+        });
+
+    };
+    return <>
+    <div className="main_div">
+        <div className="center_div">
+         <br />
+         <h1>To Do List</h1>  
+         <br/>
+          <input type="text" 
+          placeholder='Add a Items'
+          value={inputList}
+          onChange={itemEvent} />
+          <button onClick={listOfItems} >+</button>
+
+          <ol>
+            {  items.map( (itemval, index) => {
+               return <Todolist 
+               key={index} 
+               id={index}
+               text ={itemval}
+               onSelect={deleteItem} 
+                />;
+              })
+              }
+          </ol>
+        </div>
+    </div>
+    </>;
+
+};
+
+export default App;
---
+++ src/Todolist.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+
+const Todolist = (props) => {
+   
+   return(
+   <>
+   <div className="todo_style">
+      <i className="fa fa-times" aria-hidden="true" 
+      onClick={() => {
+         props.onSelect(props.id);
+      }}
+      /> 
+   <li> {props.text} </li>
+   </div>
+   </>
+    );
+}
+export default  Todolist;
